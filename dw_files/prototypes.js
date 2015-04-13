2dc3682ba2ea2b5bfc09fffe052e1c7d507eeed5
Kalkut <berenger.makita@gmail.com>
2015-04-13 14:12:09 +0200

    dw.html new url

--- dw_files/prototypes.js
+++
@@ -1,7 +0,0 @@
-Function.prototype.curry = function () {// A tasty exotic function partial evaluation method --> no more closure needed !
-	var self = this;
-	var args = Array.prototype.slice.call(arguments);
-	return function () {
-		return self.apply([], args.concat(Array.prototype.slice.call(arguments)));
-	}
-}
